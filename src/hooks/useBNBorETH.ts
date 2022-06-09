3dd3c5aed6e5c65117ddb6c9d0c20fb46d592d24
Alexandr <alexpryp@gmail.com>
2022-06-09 10:52:58 +0300

    EDIT | removing redundant components

--- src/hooks/useBNBorETH.ts
+++
@@ -1,6 +0,0 @@
-import { useActiveWeb3React } from './index'
-
-export default function useBNBorETH() {
-  const { chainId } = useActiveWeb3React()
-  return chainId == 97 || chainId == 56 ? 'BNB' : 'ETH'
-}
--- src/hooks/useConvertToHuman.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-
-export const useConvertToHuman = (value: string, decimals: string): number => {
-  return parseInt(value) / 10 ** parseInt(decimals)
-}
-
-export const convertToHuman = (value: string, decimals: string): number => {
-  return parseInt(value) / 10 ** parseInt(decimals)
-}
